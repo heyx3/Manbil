61a93585cf5cf8e7292f2fd994ac806bb9df4d9a
heyx3 <manning.w@husky.neu.edu>
2014-02-06 16:55:09 -0500

    Ability to rotate quads

--- Rendering/Helper Classes/DrawingQuad.h
+++ Rendering/Helper Classes/DrawingQuad.h
@@ -16,6 +16,8 @@ public:
     void SetPos(Vector2f pos) { quad.Transform.SetPosition(Vector3f(pos.x, pos.y, 0.0f)); }
     void SetSize(Vector2f size) { quad.Transform.SetScale(Vector3f(size.x, size.y, 1.0f)); }
     void SetDepth(float depth) { Vector3f pos = quad.Transform.GetPosition(); quad.Transform.SetPosition(Vector3f(pos.x, pos.y, depth)); }
+    void SetRotation(float newRot) { quad.Transform.SetRotation(Vector3f(0.0f, 0.0f, newRot)); }
+    void Rotate(float amount) { quad.Transform.Rotate(Vector3f(0.0f, 0.0f, amount)); }
 
     DrawingQuad(void);
     DrawingQuad(const DrawingQuad & cpy);
