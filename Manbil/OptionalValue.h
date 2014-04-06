ea66214eb9fe141174354f944e384201fd31b3d5
heyx3 <manning.w@husky.neu.edu>
2014-04-06 15:15:30 -0400

    Fixed bug with OptionalValue

--- Manbil/OptionalValue.h
+++ Manbil/OptionalValue.h
@@ -9,10 +9,11 @@ struct OptionalValue
 public:
 
     bool HasValue(void) const { return hasValue; }
+    bool SetValue(const Value & val) const { value = val; }
     Value GetValue(void) const { assert(hasValue); return value; }
 
     OptionalValue(void) : hasValue(false) { }
-    OptionalValue(const Value & valueCopy) : value(valueCopy) { }
+    OptionalValue(const Value & valueCopy) : hasValue(true), value(valueCopy) { }
 
 private:
 
