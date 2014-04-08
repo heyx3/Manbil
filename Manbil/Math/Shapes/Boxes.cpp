fe06423f0f9f54d465600866dad190af4fce2c5b
heyx3 <manning.w@husky.neu.edu>
2014-04-08 01:42:48 -0400

    Took math from RayTracer (except shapes)

--- Manbil/Math/Shapes/Boxes.cpp
+++ Manbil/Math/Shapes/Boxes.cpp
@@ -1,5 +1,8 @@
 #include "Boxes.h"
 
+
+//TODO: Optimize by replacing use of x/y/z intervals with direct code.
+
 //Basic initialized value.
 float Box2D::GeometricError = 0.01f;
 
@@ -26,37 +29,35 @@ bool Box2D::FixDimensions()
 
 bool Box2D::Touches(const Box2D & other) const
 {
-	return GetXInterval().Touches(other.GetXInterval()) &&
-		   GetYInterval().Touches(other.GetYInterval());
+    float xMinO = other.GetXMin(),
+          xMaxO = other.GetXMax(),
+          yMinO = other.GetYMin(),
+          yMaxO = other.GetYMax(),
+          xMinT = GetXMin(),
+          xMaxT = GetXMax(),
+          yMinT = GetYMin(),
+          yMaxT = GetYMax();
+    return (((xMinO >= xMinT && xMinO <= xMaxT) || (xMaxO >= xMinT && xMaxO <= xMaxT)) &&
+            ((yMinO >= yMinT && yMinO <= yMaxT) || (yMaxO >= yMinT && yMaxO <= yMaxT))) ||
+           (((xMinT >= xMinO && xMinT <= xMaxO) || (xMaxT >= xMinO && xMaxT <= xMaxO)) &&
+            ((yMinT >= yMinO && yMinT <= yMaxO) || (yMaxT >= yMinO && yMaxT <= yMaxO)));
 }
 bool Box2D::IsInside(const Box2D & other) const
 {
-	return other.IsPointInside(GetTopLeft()) &&
-		   other.IsPointInside(GetTopRight()) &&
-		   other.IsPointInside(GetBottomLeft()) &&
-		   other.IsPointInside(GetBottomRight());
+    Vector2f min = GetMinCorner(),
+             max = GetMaxCorner();
+	return other.IsPointInside(min) &&
+		   other.IsPointInside(Vector2f(min.x, max.y)) &&
+		   other.IsPointInside(Vector2f(max.x, min.y)) &&
+		   other.IsPointInside(max);
 }
 
-bool Box2D::PointTouches(Vector2f point) const
-{
-	return (GreaterThanOrEqual(point.x, GetLeftSide()) &&
-			GreaterThanOrEqual(point.y, GetTopSide()) &&
-			LessThanOrEqual(point.x, GetRightSide()) &&
-			LessThanOrEqual(point.y, GetBottomSide()));
-}
-bool Box2D::IsPointInside(Vector2f point) const
-{
-	return (GreaterThan(point.x, GetLeftSide()) &&
-			GreaterThan(point.y, GetTopSide()) &&
-			LessThan(point.x, GetRightSide()) &&
-			LessThan(point.y, GetBottomSide()));
-}
 bool Box2D::IsPointOnEdge(Vector2f point) const
 {
-	return ((WithinError(GetLeftSide(), point.x) || WithinError(GetRightSide(), point.x)) &&
-			(GetYInterval().Touches(point.y))) ||
-		   ((WithinError(GetTopSide(), point.y) || WithinError(GetBottomSide(), point.y)) &&
-			(GetXInterval().Touches(point.x)));
+	return ((WithinError(GetXMin(), point.x) || WithinError(GetXMax(), point.x)) &&
+			(point.y >= GetYMin() && point.y <= GetYMax())) ||
+		   ((WithinError(GetYMin(), point.y) || WithinError(GetYMax(), point.y)) &&
+			(point.x >= GetXMin() && point.x <= GetXMax()));
 }
 
 bool Box2D::IsEqual(const Box2D & other) const
@@ -78,21 +79,21 @@ bool Box3D::FixDimensions()
 
 	if (dimensions.x < 0.0f)
 	{
-		topLeftFront.x += dimensions.x;
+		minCorner.x += dimensions.x;
 		dimensions.x = -dimensions.x;
 		neededFixing = true;
 	}
 
 	if (dimensions.y < 0.0f)
 	{
-		topLeftFront.y += dimensions.y;
+		minCorner.y += dimensions.y;
 		dimensions.y = -dimensions.y;
 		neededFixing = true;
 	}
 
 	if (dimensions.z < 0.0f)
 	{
-		topLeftFront.z += dimensions.z;
+		minCorner.z += dimensions.z;
 		dimensions.z = -dimensions.z;
 		neededFixing = true;
 	}
@@ -102,58 +103,84 @@ bool Box3D::FixDimensions()
 
 bool Box3D::Touches(const Box3D & other) const
 {
+    float xMinO = other.GetXMin(),
+        xMaxO = other.GetXMax(),
+        yMinO = other.GetYMin(),
+        yMaxO = other.GetYMax(),
+        zMinO = other.GetZMin(),
+        zMaxO = other.GetZMax(),
+        xMinT = GetXMin(),
+        xMaxT = GetXMax(),
+        yMinT = GetYMin(),
+        yMaxT = GetYMax(),
+        zMinT = GetZMin(),
+        zMaxT = GetZMax();
+    return (((xMinO >= xMinT && xMinO <= xMaxT) || (xMaxO >= xMinT && xMaxO <= xMaxT)) &&
+            ((yMinO >= yMinT && yMinO <= yMaxT) || (yMaxO >= yMinT && yMaxO <= yMaxT)) &&
+            ((zMinO >= zMinT && zMinO <= zMaxT) || (zMaxO >= zMinT && zMaxO <= zMaxT))) ||
+           (((xMinT >= xMinO && xMinT <= xMaxO) || (xMaxT >= xMinO && xMaxT <= xMaxO)) &&
+            ((yMinT >= yMinO && yMinT <= yMaxO) || (yMaxT >= yMinO && yMaxT <= yMaxO)) &&
+            ((zMinT >= zMinO && zMinT <= zMaxO) || (zMaxT >= zMinO && zMaxT <= zMaxO)));
 	return GetXInterval().Touches(other.GetXInterval()) &&
 		   GetYInterval().Touches(other.GetYInterval()) &&
 		   GetZInterval().Touches(other.GetZInterval());
 }
 bool Box3D::IsInside(const Box3D & other) const
 {
-	return other.IsPointInside(GetTopLeftFront()) &&
-		   other.IsPointInside(GetTopRightFront()) &&
-		   other.IsPointInside(GetBottomLeftFront()) &&
-		   other.IsPointInside(GetBottomRightFront()) &&
-		   other.IsPointInside(GetTopLeftBack()) &&
-		   other.IsPointInside(GetTopRightBack()) &&
-		   other.IsPointInside(GetBottomLeftBack()) &&
-		   other.IsPointInside(GetBottomRightBack());
+    return other.IsPointInside(GetMinCorner()) &&
+           other.IsPointInside(GetMaxCorner());
+    Vector3f min = GetMinCorner(),
+             max = GetMaxCorner();
+	return other.IsPointInside(min) &&
+		   other.IsPointInside(Vector3f(min.x, min.y, max.z)) &&
+           other.IsPointInside(Vector3f(min.x, max.y, min.z)) &&
+           other.IsPointInside(Vector3f(min.x, max.y, max.z)) &&
+           other.IsPointInside(Vector3f(max.x, min.y, min.z)) &&
+           other.IsPointInside(Vector3f(max.x, min.y, max.z)) &&
+           other.IsPointInside(Vector3f(max.x, max.y, min.z)) &&
+		   other.IsPointInside(max);
 }
 
 bool Box3D::PointTouches(Vector3f point) const
 {
-	return (GreaterThanOrEqual(point.x, GetLeftSide()) &&
-			GreaterThanOrEqual(point.y, GetTopSide()) &&
-			GreaterThanOrEqual(point.z, GetFrontSide()) &&
-			LessThanOrEqual(point.x, GetRightSide()) &&
-			LessThanOrEqual(point.y, GetBottomSide()) &&
-			LessThanOrEqual(point.z, GetBackSide()));
+    return (point.x >= GetXMin() && point.y >= GetYMin() && point.z >= GetZMin() &&
+            point.x <= GetXMax() && point.y <= GetYMax() && point.z <= GetZMax());
+	return (GreaterThanOrEqual(point.x, GetXMin()) &&
+			GreaterThanOrEqual(point.y, GetYMin()) &&
+			GreaterThanOrEqual(point.z, GetZMin()) &&
+			LessThanOrEqual(point.x, GetXMax()) &&
+			LessThanOrEqual(point.y, GetYMax()) &&
+			LessThanOrEqual(point.z, GetZMax()));
 }
 bool Box3D::IsPointInside(Vector3f point) const
 {
-	return (GreaterThan(point.x, GetLeftSide()) &&
-			GreaterThan(point.y, GetTopSide()) &&
-			GreaterThan(point.z, GetFrontSide()) &&
-			LessThan(point.x, GetRightSide()) &&
-			LessThan(point.y, GetBottomSide()) &&
-			LessThan(point.z, GetBackSide()));
+    return (point.x > GetXMin() && point.y > GetYMin() && point.z > GetZMin() &&
+            point.x < GetXMax() && point.y < GetYMax() && point.z < GetZMax());
+	return (GreaterThan(point.x, GetXMin()) &&
+			GreaterThan(point.y, GetYMin()) &&
+			GreaterThan(point.z, GetZMin()) &&
+			LessThan(point.x, GetXMax()) &&
+			LessThan(point.y, GetYMax()) &&
+			LessThan(point.z, GetZMax()));
 }
 bool Box3D::IsPointOnFace(Vector3f point) const
 {
-	return ((WithinError(GetLeftSide(), point.x) || WithinError(GetRightSide(), point.x)) &&
-			GetYInterval().Touches(point.y) &&
-			GetZInterval().Touches(point.z)) ||
-		   ((WithinError(GetTopSide(), point.y) || WithinError(GetBottomSide(), point.y)) &&
-			GetXInterval().Touches(point.x) &&
-			GetZInterval().Touches(point.z)) ||
-		   ((WithinError(GetFrontSide(), point.z) || WithinError(GetBackSide(), point.z)) &&
-		    GetXInterval().Touches(point.x) &
-			GetYInterval().Touches(point.y));
+	return ((WithinError(GetXMin(), point.x) || WithinError(GetXMax(), point.x)) &&
+			(point.y >= GetYMin() && point.y <= GetYMax()) &&
+			(point.z >= GetZMin() && point.z <= GetZMax())) ||
+		   ((WithinError(GetYMin(), point.y) || WithinError(GetYMax(), point.y)) &&
+			(point.x >= GetXMin() && point.x <= GetXMax()) &&
+            (point.z >= GetZMin() && point.z <= GetZMax())) ||
+		   ((WithinError(GetZMin(), point.z) || WithinError(GetZMax(), point.z)) &&
+            (point.x >= GetXMin() && point.x <= GetXMax()) &
+            (point.y >= GetYMin() && point.y <= GetYMax()));
 }
 
 bool Box3D::IsEqual(const Box3D & other) const
 {
-	return WithinError(topLeftFront.x, other.topLeftFront.x) &&
-		   WithinError(topLeftFront.y, other.topLeftFront.y) &&
-		   WithinError(topLeftFront.z, other.topLeftFront.z) &&
+	return WithinError(minCorner.x, other.minCorner.x) &&
+		   WithinError(minCorner.y, other.minCorner.y) &&
+		   WithinError(minCorner.z, other.minCorner.z) &&
 		   WithinError(dimensions.x, other.dimensions.x) &&
 		   WithinError(dimensions.y, other.dimensions.y) &&
 		   WithinError(dimensions.z, other.dimensions.z);
--- Manbil/Math/Shapes/Boxes.h
+++ Manbil/Math/Shapes/Boxes.h
@@ -1,89 +1,107 @@
 #pragma once
 
 #include <math.h>
-#include "../LowerMath.hpp"
+#include "../Vectors.h"
+#include "../Interval.h"
 
 //Represents a rectangle shape useful for 2D collision.
 class Box2D
 {
 public:
-	
-	//The margin of error for comparing equality between two floats.
-	//This is generally bad practice, but the margin of error must be consistent across all rectangles and must be choosable by the user of this code, so I made it a public static variable.
-	//It is up to the user whether or not this error margin should be an invariant (e.x. he may want to change it in between levels for some reason).
-	static float GeometricError;
 
-	//Makes a rectangle from the given center position and dimensions.
-	static void RectByCenter(Box2D & out, float centerX, float centerY, float width, float height) { out.x = centerX - (width * 0.5f); out.y = centerY - (height * 0.5f); out.width = width; out.height = height; }
-	//Makes a rectangle from the given top-left-corner position and dimensions.
-	Box2D(float x, float y, float width, float height) { this->x = x; this->y = y; this->width = width; this->height = height; }
-	//Default constructor makes an empty Box2D.
-	Box2D() { x = 0; y = 0; width = 0; height = 0; }
-	
-	//If the rectangle has a negative width/height, flips it to make it positive.
-	//Returns whether it needed to be flipped at all.
-	bool FixDimensions();
+    //The margin of error for comparing equality between two floats when testing Box3D intersections.
+    static float GeometricError;
 
-	//Finds if this Box2D touches the given one.
-	bool Touches(const Box2D & other) const;
-	//Finds if this Box2D is inside the given one.
-	bool IsInside(const Box2D & other) const;
+    //Makes a rectangle from the given top-left corner pos and the given dimensions.
+    Box2D(float left, float top, Vector2f _dimensions)
+        : x(left), y(top), width(_dimensions.x), height(_dimensions.y)
+    {
+        width = BasicMath::Abs(width);
+        height = BasicMath::Abs(height);
+    }
+    //Makes a rectangle from the given center position and dimensions.
+    Box2D(Vector2f center, Vector2f _dimensions)
+        : Box2D(center.x - (_dimensions.x * 0.5f), center.y - (_dimensions.y * 0.5f), _dimensions)
+    {
 
-	//Finds if the given point touches this Box2D (on its edge or inside it).
-	bool PointTouches(Vector2f point) const;
-	//Finds if the given point is inside this Box2D.
-	bool IsPointInside(Vector2f point) const;
-	//Finds if the given point is on an edge of this Box2D.
-	bool IsPointOnEdge(Vector2f point) const;
+    }
+    Box2D(float left, float right, float top, float bottom)
+        : Box2D(left, top, Vector2f(right - left, bottom - top))
+    {
 
-	//Finds if this Box2D is equal to the given one.
-	bool IsEqual(const Box2D & other) const;
+    }
 
 
-	//Getters/setters.
+    //If the rectangle has a negative width/height, flips it to make it positive.
+    //Returns whether it needed to be flipped at all.
+    bool FixDimensions();
 
-	Vector2f GetPosition(void) const { return Vector2f(x, y); }
-	void SetCenterOfBox(Vector2f pos) { x = pos.x - (width * 0.5f); y = pos.y - (height * 0.5f); }
-	void Move(float deltaX, float deltaY) { x += deltaX; y += deltaY; }
+    //Finds if this Box2D touches the given one.
+    bool Touches(const Box2D & other) const;
+    //Finds if this Box2D is inside the given one.
+    bool IsInside(const Box2D & other) const;
 
-	float GetWidth(void) const { return width; }
-	float GetHeight(void) const { return height; }
-	Vector2f GetDimensions(void) const { return Vector2f(width, height); }
-	void SetWidth(float newWidth) { width = newWidth; }
-	void SetHeight(float newHeight) { height = newHeight; }
-	void SetDimensions(Vector2f newWidthAndHeight) { width = newWidthAndHeight.x; height = newWidthAndHeight.y; }
+    //Finds if the given point touches this Box2D (on its edge or inside it).
+    bool PointTouches(Vector2f point) const
+    {
+        return (point.x >= GetXMin() && point.y >= GetYMin() &&
+                point.x <= GetXMax() && point.y <= GetYMax());
+    }
+    //Finds if the given point is inside this Box2D.
+    bool IsPointInside(Vector2f point) const
+    {
+        return (point.x > GetXMin() && point.y > GetYMin() &&
+                point.x < GetXMax() && point.y < GetYMax());
+    }
+    //Finds if the given point is on an edge of this Box2D.
+    bool IsPointOnEdge(Vector2f point) const;
 
-	float GetLeftSide(void) const { return x; }
-	float GetRightSide(void) const { return x + width; }
-	float GetTopSide(void) const { return y; }
-	float GetBottomSide(void) const { return y + height; }
+    //Finds if this Box2D is equal to the given one, within the static GeometricError value.
+    bool IsEqual(const Box2D & other) const;
 
-	Vector2f GetTopLeft(void) const { return Vector2f(GetLeftSide(), GetTopSide()); }
-	Vector2f GetTopRight(void) const { return Vector2f(GetRightSide(), GetTopSide()); }
-	Vector2f GetBottomLeft(void) const { return Vector2f(GetLeftSide(), GetBottomSide()); }
-	Vector2f GetBottomRight(void) const { return Vector2f(GetRightSide(), GetBottomSide()); }
 
-	float GetCenterX(void) const { return x + (width * 0.5f); }
-	float GetCenterY(void) const { return y + (height * 0.5f); }
-	Vector2f GetCenter(void) const { return Vector2f(GetCenterX(), GetCenterY()); }
+    //Getters/setters.
 
-	Interval GetXInterval(void) const { return Interval(GetLeftSide(), GetRightSide(), 0.001f, true, true); }
-	Interval GetYInterval(void) const { return Interval(GetTopSide(), GetBottomSide(), 0.001f, true, true); }
+    Vector2f GetPosition(void) const { return Vector2f(x, y); }
+    void SetCenterOfBox(Vector2f pos) { x = pos.x - (width * 0.5f); y = pos.y - (height * 0.5f); }
+    void Move(float deltaX, float deltaY) { x += deltaX; y += deltaY; }
+
+    float GetXSize(void) const { return width; }
+    float GetYSize(void) const { return height; }
+    Vector2f GetDimensions(void) const { return Vector2f(width, height); }
+    void SetWidth(float newWidth) { width = newWidth; }
+    void SetHeight(float newHeight) { height = newHeight; }
+    void SetDimensions(Vector2f newWidthAndHeight) { width = newWidthAndHeight.x; height = newWidthAndHeight.y; }
+
+    float GetXMin(void) const { return x; }
+    float GetXMax(void) const { return x + width; }
+    float GetYMin(void) const { return y; }
+    float GetYMax(void) const { return y + height; }
+
+    Vector2f GetMinCorner(void) const { return Vector2f(GetXMin(), GetYMin()); }
+    Vector2f GetMaxCorner(void) const { return Vector2f(GetXMax(), GetYMax()); }
+
+    float GetCenterX(void) const { return x + (width * 0.5f); }
+    float GetCenterY(void) const { return y + (height * 0.5f); }
+    Vector2f GetCenter(void) const { return Vector2f(GetCenterX(), GetCenterY()); }
+
+    Interval GetXInterval(void) const { return Interval(GetXMin(), GetXMax(), 0.001f, true, true); }
+    Interval GetYInterval(void) const { return Interval(GetYMin(), GetYMax(), 0.001f, true, true); }
 
 private:
 
-	float x, y, width, height;
-	
-	//Finds if the given floats are within "GeometricError" from each other.
-	static bool WithinError(float f1, float f2) { return BasicMath::Abs(f1 - f2) <= GeometricError; }
-	//Finds if the first value is greater than the second value or within error of it.
-	static bool GreaterThanOrEqual(float f1, float f2) { return (WithinError(f1, f2) || f1 > f2); }
-	//Finds if the first value is less than the second value or within error of it.
-	static bool LessThanOrEqual(float f1, float f2) { return (WithinError(f1, f2) || f1 < f2); }
-	//Finds if the first value is greater than the second value and NOT within error of it.
-	static bool GreaterThan(float f1, float f2) { return (!WithinError(f1, f2) && f1 > f2); }
-	//Finds if the first value is less than the second value and NOT within error of it.
-	static bool LessThan(float f1, float f2) { return (!WithinError(f1, f2) && f1 < f2); }
+    float x, y, width, height;
+
+    //Finds if the given floats are within "GeometricError" from each other.
+    static bool WithinError(float f1, float f2) { return BasicMath::Abs(f1 - f2) <= GeometricError; }
+    //Finds if the first value is greater than the second value or within error of it.
+    static bool GreaterThanOrEqual(float f1, float f2) { return (WithinError(f1, f2) || f1 > f2); }
+    //Finds if the first value is less than the second value or within error of it.
+    static bool LessThanOrEqual(float f1, float f2) { return (WithinError(f1, f2) || f1 < f2); }
+    //Finds if the first value is greater than the second value and NOT within error of it.
+    static bool GreaterThan(float f1, float f2) { return (!WithinError(f1, f2) && f1 > f2); }
+    //Finds if the first value is less than the second value and NOT within error of it.
+    static bool LessThan(float f1, float f2) { return (!WithinError(f1, f2) && f1 < f2); }
 
 };
 
@@ -91,89 +109,98 @@ private:
 class Box3D
 {
 public:
-	
-	//The margin of error for comparing equality between two floats.
-	//This is generally bad practice, but the margin of error must be consistent across all Box3D's and must be choosable by the user of this code, so I made it a public static variable.
-	//It is up to the user whether or not this error margin should be an invariant (e.x. he may want to change it in between levels for some reason).
-	static float GeometricError;
 
-	//Makes a rectangle from the given center position and dimensions.
-	static void RectByCenter(Box3D & out, Vector3f center, Vector3f dimensions) { out.topLeftFront = center - (dimensions * 0.5f); out.dimensions = dimensions; }
-	//Makes a rectangle from the given top-left-front corner and dimensions.
-	Box3D(Vector3f topLeftFrontCorner, Vector3f _dimensions): topLeftFront(topLeftFrontCorner), dimensions(_dimensions) { }
-	
-	//If the rectangle has a negative width/height/depth, flips it to make it positive.
-	//Returns whether it needed to be flipped at all.
-	bool FixDimensions();
-
-	//Finds if this Box3D touches the given one.
-	bool Touches(const Box3D & other) const;
-	//Finds if this Box3D is inside the given one.
-	bool IsInside(const Box3D & other) const;
-
-	//Finds if the given point touches this Box3D (on its edge or inside it).
-	bool PointTouches(Vector3f point) const;
-	//Finds if the given point is inside this Box3D.
-	bool IsPointInside(Vector3f point) const;
-	//Finds if the given point is on a face of this Box3D.
-	bool IsPointOnFace(Vector3f point) const;
-
-	//Finds if this Box3D is equal to the given one.
-	bool IsEqual(const Box3D & other) const;
+    //The margin of error for comparing equality between two floats when testing Box3D intersections.
+    static float GeometricError;
 
 
-	//Getters/setters.
+    //Makes a rectangle from the given top-front-left corner pos and the given dimensions.
+    Box3D(float minX, float minY, float minZ, Vector3f _dimensions)
+        : minCorner(Vector3f(minX, minY, minZ)), dimensions(_dimensions)
+    {
+        dimensions = Vector3f(BasicMath::Abs(dimensions.x), BasicMath::Abs(dimensions.y), BasicMath::Abs(dimensions.z));
+    }
+    //Makes a rectangle from the given center position and dimensions.
+    Box3D(Vector3f center, Vector3f _dimensions)
+        : Box3D(center.x - (_dimensions.x * 0.5f), center.y - (_dimensions.y * 0.5f), center.z - (_dimensions.z * 0.5f), _dimensions)
+    {
 
-	void SetCenterOfBox(Vector3f pos) { topLeftFront += (pos - GetCenter()); }
-	void Move(Vector3f amount) { topLeftFront += amount; }
+    }
+    //Makes a rectangle from the given border values.
+    Box3D(float minX, float maxX, float minY, float maxY, float minZ, float maxZ)
+        : Box3D(minX, minY, minZ, Vector3f(maxX - minX, maxY - minY, maxZ - minZ))
+    {
 
-	float GetWidth(void) const { return dimensions.x; }
-	float GetHeight(void) const { return dimensions.y; }
-	float GetDepth(void) const { return dimensions.z; }
-	Vector3f GetDimensions(void) const { return dimensions; }
-	void SetWidth(float newWidth) { dimensions.x = newWidth; }
-	void SetHeight(float newHeight) { dimensions.y = newHeight; }
-	void SetDepth(float newDepth) { dimensions.z = newDepth; }
-	void SetDimensions(Vector3f newDimensions) { dimensions = newDimensions; }
+    }
+    Box3D(void) : Box3D(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f) { }
 
-	float GetLeftSide(void) const { return topLeftFront.x; }
-	float GetRightSide(void) const { return topLeftFront.x + dimensions.x; }
-	float GetTopSide(void) const { return topLeftFront.y; }
-	float GetBottomSide(void) const { return topLeftFront.y + dimensions.y; }
-	float GetFrontSide(void) const { return topLeftFront.z; }
-	float GetBackSide(void) const { return topLeftFront.z + dimensions.z; }
 
-	Vector3f GetTopLeftFront(void) const { return topLeftFront; }
-	Vector3f GetTopLeftBack(void) const { return Vector3f(topLeftFront.x, topLeftFront.y, topLeftFront.z + dimensions.z); }
-	Vector3f GetTopRightFront(void) const { return Vector3f(topLeftFront.x + dimensions.x, topLeftFront.y, topLeftFront.z); }
-	Vector3f GetTopRightBack(void) const { return Vector3f(topLeftFront.x + dimensions.x, topLeftFront.y, topLeftFront.z + dimensions.z); }
-	Vector3f GetBottomLeftFront(void) const { return Vector3f(topLeftFront.x, topLeftFront.y + dimensions.y, topLeftFront.z); }
-	Vector3f GetBottomLeftBack(void) const { return Vector3f(topLeftFront.x, topLeftFront.y + dimensions.y, topLeftFront.z + dimensions.z); }
-	Vector3f GetBottomRightFront(void) const { return Vector3f(topLeftFront.x + dimensions.x, topLeftFront.y + dimensions.y, topLeftFront.z); }
-	Vector3f GetBottomRightBack(void) const { return Vector3f(topLeftFront.x + dimensions.x, topLeftFront.y + dimensions.y, topLeftFront.z + dimensions.z); }
-	
-	float GetCenterX(void) const { return topLeftFront.x + (0.5f * dimensions.x); }
-	float GetCenterY(void) const { return topLeftFront.y + (0.5f * dimensions.y); }
-	float GetCenterZ(void) const { return topLeftFront.z + (0.5f * dimensions.z); }
-	Vector3f GetCenter(void) const { return Vector3f(GetCenterX(), GetCenterY(), GetCenterZ()); }
+    //If the rectangle has a negative width/height/depth, flips it to make it positive.
+    //Returns whether it needed to be flipped at all.
+    bool FixDimensions();
 
-	Interval GetXInterval(void) const { return Interval(GetLeftSide(), GetRightSide(), 0.001f, true, true); }
-	Interval GetYInterval(void) const { return Interval(GetTopSide(), GetBottomSide(), 0.001f, true, true); }
-	Interval GetZInterval(void) const { return Interval(GetFrontSide(), GetBackSide(), 0.001f, true, true); }
+    //Finds if this Box3D touches the given one.
+    bool Touches(const Box3D & other) const;
+    //Finds if this Box3D is inside the given one.
+    bool IsInside(const Box3D & other) const;
+
+    //Finds if the given point touches this Box3D (on its edge or inside it).
+    bool PointTouches(Vector3f point) const;
+    //Finds if the given point is inside this Box3D.
+    bool IsPointInside(Vector3f point) const;
+    //Finds if the given point is on a face of this Box3D.
+    bool IsPointOnFace(Vector3f point) const;
+
+    //Finds if this Box3D is equal to the given one, within the static GeometricError value.
+    bool IsEqual(const Box3D & other) const;
+
+
+    //Getters/setters.
+
+    void SetCenterOfBox(Vector3f pos) { minCorner += (pos - GetCenter()); }
+    void Move(Vector3f amount) { minCorner += amount; }
+
+    float GetXSize(void) const { return dimensions.x; }
+    float GetYSize(void) const { return dimensions.y; }
+    float GetZSize(void) const { return dimensions.z; }
+    Vector3f GetDimensions(void) const { return dimensions; }
+    void SetWidth(float newWidth) { dimensions.x = newWidth; }
+    void SetHeight(float newHeight) { dimensions.y = newHeight; }
+    void SetDepth(float newDepth) { dimensions.z = newDepth; }
+    void SetDimensions(Vector3f newDimensions) { dimensions = newDimensions; }
+
+    float GetXMin(void) const { return minCorner.x; }
+    float GetXMax(void) const { return minCorner.x + dimensions.x; }
+    float GetYMin(void) const { return minCorner.y; }
+    float GetYMax(void) const { return minCorner.y + dimensions.y; }
+    float GetZMin(void) const { return minCorner.z; }
+    float GetZMax(void) const { return minCorner.z + dimensions.z; }
+
+    Vector3f GetMinCorner(void) const { return minCorner; }
+    Vector3f GetMaxCorner(void) const { return minCorner + dimensions; }
+
+    float GetCenterX(void) const { return minCorner.x + (0.5f * dimensions.x); }
+    float GetCenterY(void) const { return minCorner.y + (0.5f * dimensions.y); }
+    float GetCenterZ(void) const { return minCorner.z + (0.5f * dimensions.z); }
+    Vector3f GetCenter(void) const { return Vector3f(GetCenterX(), GetCenterY(), GetCenterZ()); }
+
+    Interval GetXInterval(void) const { return Interval(GetXMin(), GetXMax(), 0.001f, true, true); }
+    Interval GetYInterval(void) const { return Interval(GetYMin(), GetYMax(), 0.001f, true, true); }
+    Interval GetZInterval(void) const { return Interval(GetZMin(), GetZMax(), 0.001f, true, true); }
 
 private:
 
-	Vector3f topLeftFront, dimensions;
-	
-	//Finds if the given floats are within "GeometricError" from each other.
-	static bool WithinError(float f1, float f2) { return BasicMath::Abs(f1 - f2) <= GeometricError; }
-	//Finds if the first value is greater than the second value or within error of it.
-	static bool GreaterThanOrEqual(float f1, float f2) { return (WithinError(f1, f2) || f1 > f2); }
-	//Finds if the first value is less than the second value or within error of it.
-	static bool LessThanOrEqual(float f1, float f2) { return (WithinError(f1, f2) || f1 < f2); }
-	//Finds if the first value is greater than the second value and NOT within error of it.
-	static bool GreaterThan(float f1, float f2) { return (!WithinError(f1, f2) && f1 > f2); }
-	//Finds if the first value is less than the second value and NOT within error of it.
-	static bool LessThan(float f1, float f2) { return (!WithinError(f1, f2) && f1 < f2); }
+    Vector3f minCorner, dimensions;
+
+    //Finds if the given floats are within "GeometricError" from each other.
+    static bool WithinError(float f1, float f2) { return BasicMath::Abs(f1 - f2) <= GeometricError; }
+    //Finds if the first value is greater than the second value or within error of it.
+    static bool GreaterThanOrEqual(float f1, float f2) { return (WithinError(f1, f2) || f1 > f2); }
+    //Finds if the first value is less than the second value or within error of it.
+    static bool LessThanOrEqual(float f1, float f2) { return (WithinError(f1, f2) || f1 < f2); }
+    //Finds if the first value is greater than the second value and NOT within error of it.
+    static bool GreaterThan(float f1, float f2) { return (!WithinError(f1, f2) && f1 > f2); }
+    //Finds if the first value is less than the second value and NOT within error of it.
+    static bool LessThan(float f1, float f2) { return (!WithinError(f1, f2) && f1 < f2); }
 };
 
