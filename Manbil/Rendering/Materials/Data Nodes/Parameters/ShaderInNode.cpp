083a2d295841040e3b472a397f66d7183ac95c52
heyx3 <manning.w@husky.neu.edu>
2014-06-05 18:52:48 -0400

    Finished (not tested) ShaderInNode

---
+++ Manbil/Rendering/Materials/Data Nodes/Parameters/ShaderInNode.cpp
@@ -0,0 +1,32 @@
+#include "ShaderInNode.h"
+
+#include "../../MaterialData.h"
+
+
+std::string ShaderInNode::GetOutputName(unsigned int outputIndex) const
+{
+    Assert(outputIndex == 0, std::string() + "Output index must be 0, but it was " + std::to_string(outputIndex));
+
+    switch (GetShaderType())
+    {
+        case Shaders::SH_Vertex_Shader:
+            Assert(HasVertexInput(), "Attempted to get value of a vertex input that doesn't exist!");
+            return MaterialConstants::VertexInNameBase + std::to_string(vInputIndex);
+
+        case Shaders::SH_GeometryShader:
+            Assert(HasGeometryInput(), "Attempted to get value of a geometry input that doesn't exist!");
+            Assert(GetGeoShaderData()->IsValidData(), "Attempted to get value of a geometry input when there is no geometry shader!");
+            return MaterialConstants::VertexOutNameBase + std::to_string(gInputIndex);
+
+        case Shaders::SH_Fragment_Shader:
+            Assert(HasFragmentInput(), "Attempted to get value of a fragment input that doesn't exist!");
+            const GeoShaderData * dat = GetGeoShaderData();
+            if (dat->IsValidData())
+                return dat->OutputTypes.OutputNames[fInputIndex];
+            else return MaterialConstants::VertexOutNameBase + std::to_string(fInputIndex);
+
+        default:
+            Assert(false, std::string() + "Unknown shader type " + ToString(GetShaderType()));
+            return "ERROR_UNKNOWN_SHADER_TYPE";
+    }
+}
--- Manbil/Rendering/Materials/Data Nodes/Parameters/ShaderInNode.h
+++ Manbil/Rendering/Materials/Data Nodes/Parameters/ShaderInNode.h
@@ -8,15 +8,32 @@ class ShaderInNode : public DataNode
 {
 public:
 
-    bool HasVertexInput(void) const { return vertexInputIndex >= 0; }
-    bool HasGeometryInput(void) const { return geoInputIndex >= 0; }
-    bool HasFragmentInput(void) const { return fragInputIndex >= 0; }
+    bool HasVertexInput(void) const { return vInputIndex >= 0; }
+    bool HasGeometryInput(void) const { return gInputIndex >= 0; }
+    bool HasFragmentInput(void) const { return fInputIndex >= 0; }
 
 
+    virtual std::string GetName(void) const override { return "shaderInNode"; }
+    virtual std::string GetOutputName(unsigned int outputIndex) const override;
 
+
+    ShaderInNode(unsigned int size, int vertexInputIndex = -1, int geometryInputIndex = -1, int fragmentInputIndex = -1)
+        : vInputIndex(vertexInputIndex), gInputIndex(geometryInputIndex), fInputIndex(fragmentInputIndex),
+          DataNode(std::vector<DataLine>(), MakeVector(size))
+    {
+        Assert(size > 0, "Size of input is 0! Must be from 1-4, inclusive.");
+        Assert(size < 5, std::string() + "Size of input is " + std::to_string(size) + "! Must be from 1-4, inclusive.");
+    }
+
+protected:
+
+    virtual void WriteMyOutputs(std::string & outCode) const
+    {
+        //Don't write anything; this node outputs shader inputs.
+    }
 
 
 private:
 
-    int vertexInputIndex, geoInputIndex, fragInputIndex;
+    int vInputIndex, gInputIndex, fInputIndex;
 };
