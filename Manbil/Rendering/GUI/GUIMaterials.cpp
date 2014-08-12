9525c52c216a1db375fe825daef393ac8cc6246f
heyx3 <manning.w@husky.neu.edu>
2014-08-11 23:33:21 -0400

    Better GUIMaterials system
    Two materials -- a static non-animated material for easy use, and a
    material that interpolates the color, size, and rotation based on a
    uniform value so that the element can be tweened

--- Manbil/Rendering/GUI/GUIMaterials.cpp
+++ Manbil/Rendering/GUI/GUIMaterials.cpp
@@ -5,13 +5,17 @@
 
 const std::string GUIMaterials::QuadDraw_Color = "u_color",
                   GUIMaterials::QuadDraw_Texture2D = "u_tex2D";
+const std::string GUIMaterials::DynamicQuadDraw_TimeLerp = "u_timeLerp";
 
-ShaderGenerator::GeneratedMaterial GUIMaterials::GenerateQuadDrawMaterial(UniformDictionary & params,
+
+typedef DataNode::Ptr DNP;
+
+
+ShaderGenerator::GeneratedMaterial GUIMaterials::GenerateStaticQuadDrawMaterial(UniformDictionary & params,
                                                                           ShaderInOutAttributes vertexAttrs,
-                                                                          unsigned int posIndex, unsigned int uvIndex)
+                                                                          unsigned int posIndex,
+                                                                          unsigned int uvIndex)
 {
-    typedef DataNode::Ptr DNP;
-
     DataNode::ClearMaterialData();
     DataNode::VertexIns = DrawingQuad::GetAttributeData();
 
@@ -25,8 +29,43 @@ ShaderGenerator::GeneratedMaterial GUIMaterials::GenerateQuadDrawMaterial(Unifor
                                                              DataLine(VertexInputNode::GetInstance(), uvIndex)));
 
     DNP texSample(new TextureSample2DNode(FragmentInputNode::GetInstance(),
-                                          QuadDraw_Texture2D, "GUIMat_textSamplerNode"));
-    DNP texColor(new ParamNode(4, QuadDraw_Color, "GUIMat_textColor"));
+                                          QuadDraw_Texture2D, "GUIMat_texSamplerNode"));
+    DNP texColor(new ParamNode(4, QuadDraw_Color, "GUIMat_texColor"));
+    DNP texColored(new MultiplyNode(DataLine(texSample, TextureSample2DNode::GetOutputIndex(CO_Red)),
+                                    texColor, "GUIMat_finalColor"));
+    DataNode::MaterialOuts.FragmentOutputs.insert(DataNode::MaterialOuts.FragmentOutputs.end(),
+                                                  ShaderOutput("fOut_Color4", texColored));
+
+    return ShaderGenerator::GenerateMaterial(params, RenderingModes::RM_Transluscent);
+}
+
+ShaderGenerator::GeneratedMaterial GUIMaterials::GenerateDynamicQuadDrawMaterial(UniformDictionary & params,
+                                                                                 DataLine endScale,
+                                                                                 DataLine endColor,
+                                                                                 DataLine endRotation,
+                                                                                 ShaderInOutAttributes vertIns,
+                                                                                 unsigned int posIndex,
+                                                                                 unsigned int uvIndex)
+{
+    DataNode::ClearMaterialData();
+    DataNode::VertexIns = vertIns;
+
+    DNP vertexPosOut(new SpaceConverterNode(DataLine(VertexInputNode::GetInstance(), posIndex),
+                                            SpaceConverterNode::ST_OBJECT, SpaceConverterNode::ST_SCREEN,
+                                            SpaceConverterNode::DT_POSITION, "GUIMat_objToScreenPos"));
+    DNP rotateScreen(new RotateAroundAxisNode(vertexPosOut, Vector3f(0.0f, 0.0f, 1.0f), endRotation, "GUIMat_rotate"));
+    DNP screenScaleAmount(new CombineVectorNode(endScale, 1.0f, "GUIMat_scaleScreenAmt"));
+    DNP scaleScreen(new MultiplyNode(rotateScreen, screenScaleAmount, "GUIMat_scaleScreen"));
+    DNP finalPosOut(new CombineVectorNode(scaleScreen, 1.0f, "GUIMat_finalPosOut"));
+    DataNode::MaterialOuts.VertexPosOutput = finalPosOut;
+
+    DataNode::MaterialOuts.VertexOutputs.insert(DataNode::MaterialOuts.VertexOutputs.end(),
+                                                ShaderOutput("vOut_UV",
+                                                             DataLine(VertexInputNode::GetInstance(), uvIndex)));
+
+    DNP texSample(new TextureSample2DNode(FragmentInputNode::GetInstance(),
+                                          QuadDraw_Texture2D, "GUIMat_texSamplerNode"));
+    DNP texColor(new ParamNode(4, QuadDraw_Color, "GUIMat_texColor"));
     DNP texColored(new MultiplyNode(DataLine(texSample, TextureSample2DNode::GetOutputIndex(CO_Red)),
                                     texColor, "GUIMat_finalColor"));
     DataNode::MaterialOuts.FragmentOutputs.insert(DataNode::MaterialOuts.FragmentOutputs.end(),
