7bd97125a10089d95643c465f5301b000346aa43
heyx3 <manning.w@husky.neu.edu>
2014-09-03 12:15:37 -0400

    Added ScreenClearer to Rendering.hpp

--- Manbil/Rendering/Rendering.hpp
+++ Manbil/Rendering/Rendering.hpp
@@ -2,6 +2,7 @@
 
 #include "../Material.h"
 #include "../RenderingState.h"
+#include "../ScreenClearer.h"
 #include "Texture Management/RenderTargetManager.h"
 #include "Texture Management/MTexture2D.h"
 #include "Texture Management/MTexture3D.h"
