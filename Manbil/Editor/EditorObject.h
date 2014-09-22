f381edb73328db6dc362d9382a0b3fe269a5e083
heyx3 <manning.w@husky.neu.edu>
2014-09-22 14:31:00 -0400

    Updated EditorObject's Update() function
    Now returns a boolean indicating whether Update() failed.

--- Manbil/Editor/EditorObject.h
+++ Manbil/Editor/EditorObject.h
@@ -24,11 +24,16 @@ public:
     //If there was a problem, returns false and sets the static "ErrorMsg" field. Otherwise returns true.
     //Takes in the content to use for rendering.
     virtual bool InitGUIElement(EditorMaterialSet & materialSet) = 0;
-    //Updates this object and its element. Should NOT update this object's active GUI element.
-    //Default behavior: do nothing.
-    virtual void Update(float elapsed, Vector2f panelRelativeMousePos) { }
+    //Updates this object and its element's properties. Does NOT need to call Update() for this object's active GUI element.
+    //Returns whether the active GUI element was moved or resized at all
+    //   (i.e. whether the formatted panel has to rebuild its layout).
+    //Default behavior: do nothing, return false.
+    virtual bool Update(float elapsed, Vector2f panelRelativeMousePos) { return false; }
 
 protected:
 
     GUIElementPtr activeGUIElement;
 };
+
+
+typedef std::shared_ptr<EditorObject> EditorObjectPtr;
