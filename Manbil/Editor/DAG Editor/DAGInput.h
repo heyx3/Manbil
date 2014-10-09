a8017c7e3b35df5ae041ab3dd79b4cf705ea5d32
heyx3 <manning.w@husky.neu.edu>
2014-10-09 14:04:35 -0400

    More work on DAGNode/DAGInput

--- Manbil/Editor/DAG Editor/DAGInput.h
+++ Manbil/Editor/DAG Editor/DAGInput.h
@@ -4,6 +4,7 @@
 #include "../../IO/DataSerialization.h"
 
 
+
 //The type of simple data that can be used in place of a DAGNode.
 //Using "void" for this value results in a DATInput with no default value type.
 template<typename DefaultValueType>
@@ -14,8 +15,8 @@ class DAGInput : public ISerializable
 public:
 
 
-    DAGInput(std::string dagNodeName = "", unsigned int dagNodeOutIndex = 0)
-        : nonConstantName(dagNodeName), nonConstantOutIndex(dagNodeOutIndex) { }
+    DAGInput(std::string nodeName = "", unsigned int nodeOutIndex = 0)
+        : nonConstantName(nodeName), nonConstantOutIndex(dagNodeOutIndex) { }
     DAGInput(const DefaultValueType & constantVal)
         : constantValue(constantVal), nonConstantName(""), nonConstantOutIndex(0) { }
 
@@ -31,15 +32,16 @@ public:
     //Gets the value of this input, assuming it's constant.
     const DefaultValueType & GetConstantValue(void) const { assert(IsConstant()); return constantValue; }
     //Gets the DAG node this input points to, assuming this input isn't constant.
-    const std::string & GetDAGNodeName(void) const { assert(!IsConstant()); return nonConstantName; }
+    const std::string & GetDAGNode(void) const { assert(!IsConstant()); return nonConstantName; }
     //Gets the DAG node's output index, assuming this input isn't constant.
-    unsigned int GetDAGNodeOutputIndex(void) const { assert(!IsConstant()); return nonConstantOutIndex; }
+    unsigned int GetDAGNodeOutIndex(void) const { assert(!IsConstant()); return nonConstantOutIndex; }
 
 
     //Sets the value of this input to the given constant value.
     //If it was already pointing to a DAG node output, that will be overwritten.
     void SetConstantInput(const DefaultValueType & defVal) { nonConstantName = ""; constantValue = defVal; }
     //Sets the value of this input to the given DAG node.
+    //If it was already pointing to a constant value, that will be overwritten.
     void SetDAGNodeInput(const std::string & nodeName, unsigned int nodeOutputIndex)
     {
         nonConstantName = nodeName;
@@ -47,9 +49,39 @@ public:
     }
 
 
+    bool ReadData(DataReader * data, std::string & outError) override
+    {
+        if (IsConstant())
+        {
+            return ReadConstantData(data, outError);
+        }
+        else
+        {
+            data->ReadString()
+        }
+    }
+    bool WriteData(DataWriter * data, std::string & outError) const override
+    {
+        if (IsConstant())
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+
+protected:
+
+    virtual bool ReadConstantValue(DataReader * data, std::string & outError) = 0;
+    virtual bool WriteConstantValue(DataWriter * data, std::string & outError) = 0;
+
+
 private:
 
-    std::string nonConstantName;
+    std::string nonConstantNode;
     unsigned int nonConstantOutIndex;
 
     DefaultValueType constantValue;
@@ -62,5 +94,38 @@ private:
 template<>
 class DAGInput<void>
 {
-    
+public:
+
+
+    DAGInput(void* nodePtr = 0, unsigned int _nodeOutIndex = 0)
+        : nodeP(nodePtr), nodeOutIndex(_nodeOutIndex) { }
+
+    virtual ~DAGInput(void) { }
+
+
+    //Returns false (this type of DAGInput doesn't have a default constant value type).
+    bool IsConstant(void) const { return false; }
+
+
+    //Gets the DAG node this input points to.
+    void* GetDAGNode(void) const { return nodeP; }
+    //Gets the DAG node's output index.
+    unsigned int GetDAGNodeOutIndex(void) const { return nodeOutIndex; }
+
+    //Sets the value of this input to the given DAG node.
+    void SetDAGNodeInput(void* nodePtr, unsigned int nodeOutputIndex)
+    {
+        nodeP = nodePtr;
+        nodeOutIndex = nodeOutputIndex;
+    }
+
+
+private:
+
+    void* nodeP;
+    unsigned int nodeOutIndex;
 };
+
+
+
+DAGInput<float> dIn = DAGInput<float>(10.0f);
--- Manbil/Editor/DAG Editor/DAGNode.h
+++ Manbil/Editor/DAG Editor/DAGNode.h
@@ -1,25 +1,68 @@
 #pragma once
 
-
 #include "../../IO/DataSerialization.h"
 #include "../IEditable.h"
 #include "DAGInput.h"
 
 
+
+template<typename DAGNodeType, typename DAGInputType>
+//A base class for the visitor pattern being applied to a DAG from a root node.
+//Note that after visiting each DAG node, this visitor will remember and always skip that node.
+class DAGVisitor
+{
+public:
+
+    virtual ~DAGVisitor(void) { }
+    
+    //Clears this visitor's node history so that it forgets any nodes it's already visited.
+    void ClearNodeHistory(void) { visitedAlready.clear(); }
+    void Visit(DAGNodeType * node)
+    {
+        if (std::find(visitedAlready.begin(), visitedAlready.end(), node.GetName()) == visitedAlready.end())
+        {
+            visitedAlready.insert(visitedAlready.end(), node.GetName());
+            ActOnNode(node);
+
+            const std::vector<DAGInputType> & children = node.GetChildren();
+            for (unsigned int i = 0; i < children.size(); ++i)
+                if (!children[i].IsConstant())
+                    this->Visit((DAGNodeType*)children[i].GetDAGNode());
+        }
+    }
+
+protected:
+
+    //Visits the given node and does some action on it.
+    virtual void ActOnNode(DAGNodeType * node) = 0;
+
+private:
+
+    std::vector<void*> visitedAlready;
+};
+
+
+
 //The type of DAG connection this node uses.
 //The data type should be easily copyable.
-template<typename DAGConnectionType>
+template<typename DAGInputType>
 //A node in a DAG. Can generate a GUI to edit it and be serialized to a file.
 class DAGNode : public ISerializable, public IEditable
 {
 public:
 
 
+    DAGNode(const std::vector<DAGInputType> inputs)
+
+
+
 protected:
 
     
 
+
+
 private:
 
-    std::vector<DAGConnectionType> inputs;
+    std::vector<DAGInputType> inputs;
 };
