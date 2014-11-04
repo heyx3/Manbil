d2c5a83325c9c9d12c40e608baee41f2dbb0d07d
heyx3 <manning.w@husky.neu.edu>
2014-11-04 14:58:27 -0500

    Remade GUIPanel to work with new overhaul

--- Manbil/Rendering/GUI/GUI Elements/GUIPanel.cpp
+++ Manbil/Rendering/GUI/GUI Elements/GUIPanel.cpp
@@ -2,33 +2,49 @@
 
 
 
-void GUIPanel::RecalcPosition(void)
+Box2D GUIPanel::GetBounds(void) const
 {
-    Vector2f min, max;
+    Box2D bounds;
 
+    if (elements.empty())
+        bounds = Box2D();
+    bounds = elements[0]->GetBounds();
+    for (unsigned int i = 1; i < elements.size(); ++i)
+        bounds = Box2D(bounds, elements[i]->GetBounds());
+
+    return bounds;
+}
+bool GUIPanel::GetDidBoundsChangeDeep(void) const
+{
+    if (DidBoundsChange) return true;
+
+    for (unsigned int i = 0; i < elements.size(); ++i)
+        if (elements[i]->GetDidBoundsChangeDeep())
+            return true;
+
+    return false;
+}
+
+void GUIPanel::ScaleBy(Vector2f scaleAmount)
+{
+    GUIElement::ScaleBy(scaleAmount);
+
+    //Scale each element's position to move it relative to this panel's center.
     for (unsigned int i = 0; i < elements.size(); ++i)
     {
-        Vector2f elPos = elements[i]->GetCollisionCenter();
+        bool oldDidChange = elements[i]->DidBoundsChange;
 
-        Vector2f elDims = elements[i]->GetCollisionDimensions(),
-                 elMin = elPos - (elDims * 0.5f),
-                 elMax = elPos + (elDims * 0.5f);
-        min.x = BasicMath::Min(elMin.x, min.x);
-        min.y = BasicMath::Min(elMin.y, min.y);
-        max.x = BasicMath::Max(elMax.x, max.x);
-        max.y = BasicMath::Max(elMax.y, max.y);
+        elements[i]->SetPosition(elements[i]->GetPos().ComponentProduct(scaleAmount));
+        elements[i]->ScaleBy(scaleAmount);
+
+        elements[i]->DidBoundsChange = oldDidChange;
     }
-
-    Vector2f newCenter = (max + min) * 0.5f,
-             newSize = max - min;
-    
-
-    Vector2f delta = newCenter - pos;
-    pos = newCenter;
-    for (unsigned int i = 0; i < elements.size(); ++i)
-        elements[i]->MoveElement(delta);
-
-    extents = newSize;
+}
+void GUIPanel::SetScale(Vector2f newScale)
+{
+    Vector2f oldScale = GetScale();
+    Vector2f delta(newScale.x / oldScale.x, newScale.y / oldScale.y);
+    ScaleBy(delta);
 }
 
 void GUIPanel::AddElement(GUIElementPtr element)
@@ -36,7 +52,6 @@ void GUIPanel::AddElement(GUIElementPtr element)
     if (std::find(elements.begin(), elements.end(), element) == elements.end())
     {
         elements.insert(elements.end(), element);
-        RecalcPosition();
     }
 }
 bool GUIPanel::RemoveElement(GUIElementPtr element)
@@ -46,7 +61,6 @@ bool GUIPanel::RemoveElement(GUIElementPtr element)
         return false;
 
     elements.erase(loc);
-    RecalcPosition();
 
     return true;
 }
@@ -55,38 +69,16 @@ bool GUIPanel::ContainsElement(GUIElementPtr element) const
     return (std::find(elements.begin(), elements.end(), element) != elements.end());
 }
 
-void GUIPanel::ScaleBy(Vector2f scaleAmount)
-{
-    //Scale the 'extents' vector.
-    extents.MultiplyComponents(scaleAmount);
-
-    //Scale each element's position to move it relative to this panel's center.
-    for (unsigned int i = 0; i < elements.size(); ++i)
-    {
-        Vector2f elPos = elements[i]->GetCollisionCenter();
-        elements[i]->SetPosition(elPos.ComponentProduct(scaleAmount));
-        elements[i]->ScaleBy(scaleAmount);
-    }
-}
-void GUIPanel::SetScale(Vector2f newScale)
-{
-    Vector2f dims = GetCollisionDimensions();
-    Vector2f delta(newScale.x / dims.x, newScale.y / dims.y);
-    ScaleBy(delta);
-}
-
 void GUIPanel::CustomUpdate(float elapsed, Vector2f relativeMousePos)
 {
     for (unsigned int i = 0; i < elements.size(); ++i)
     {
-        Vector2f relPos = relativeMousePos - elements[i]->GetCollisionCenter();
+        Vector2f relPos = relativeMousePos - elements[i]->GetPos();
         elements[i]->Update(elapsed, relPos);
     }
 }
 std::string GUIPanel::Render(float elapsedTime, const RenderInfo & info)
 {
-    Vector2f nPos = -pos;
-
     //Instead of returning an error as soon as it is found,
     //    render all sub-elements and collect any errors into one big error string.
     std::string err = "";
@@ -94,12 +86,7 @@ std::string GUIPanel::Render(float elapsedTime, const RenderInfo & info)
 
     for (unsigned int i = 0; i < elements.size(); ++i)
     {
-        elements[i]->MoveElement(pos);
-        elements[i]->Depth += Depth;
-        std::string tempErr = elements[i]->Render(elapsedTime, info);
-        elements[i]->MoveElement(nPos);
-        elements[i]->Depth -= Depth;
-
+        std::string tempErr = RenderChild(elements[i].get(), elapsedTime, info);
         if (!tempErr.empty())
         {
             if (line > 0) err += "\n";
@@ -115,18 +102,18 @@ std::string GUIPanel::Render(float elapsedTime, const RenderInfo & info)
 void GUIPanel::OnMouseClick(Vector2f mouseP)
 {
     for (unsigned int i = 0; i < elements.size(); ++i)
-        elements[i]->OnMouseClick(mouseP - elements[i]->GetCollisionCenter());
+        elements[i]->OnMouseClick(mouseP - elements[i]->GetPos());
 }
 void GUIPanel::OnMouseDrag(Vector2f oldP, Vector2f currentP)
 {
     for (unsigned int i = 0; i < elements.size(); ++i)
     {
-        Vector2f center = elements[i]->GetCollisionCenter();
+        Vector2f center = elements[i]->GetPos();
         elements[i]->OnMouseDrag(oldP - center, currentP - center);
     }
 }
 void GUIPanel::OnMouseRelease(Vector2f mouseP)
 {
     for (unsigned int i = 0; i < elements.size(); ++i)
-        elements[i]->OnMouseRelease(mouseP - elements[i]->GetCollisionCenter());
+        elements[i]->OnMouseRelease(mouseP - elements[i]->GetPos());
 }
--- Manbil/Rendering/GUI/GUI Elements/GUIPanel.h
+++ Manbil/Rendering/GUI/GUI Elements/GUIPanel.h
@@ -3,26 +3,14 @@
 #include "../GUIElement.h"
 
 
-//Contains a collection of GUI elements.
+//Contains an arbitrary collection of child GUI elements.
 //TODO: If this panel is marked as "scrollable", render its elements to a texture and then render that texture, so that the elements never extend outside this panel's bounds. Also have a private GUISlider that acts as the scroll bar.
 class GUIPanel : public GUIElement
 {
 public:
 
-    GUIPanel(const UniformDictionary & params, Vector2f _extents, float timeLerpSpeed = 1.0f)
-        : extents(_extents), GUIElement(params, timeLerpSpeed) { }
-
-
-    virtual Vector2f GetCollisionCenter(void) const override { return pos; }
-    virtual Vector2f GetCollisionDimensions(void) const override { return extents; }
-
-    virtual void MoveElement(Vector2f moveAmount) override { pos += moveAmount; }
-    virtual void SetPosition(Vector2f newPos) override { pos = newPos; }
-
-    virtual Vector2f GetScale(void) const override { return extents; }
-
-    virtual void ScaleBy(Vector2f scaleAmount) override;
-    virtual void SetScale(Vector2f newScale) override;
+    GUIPanel(const UniformDictionary & params, float timeLerpSpeed = 1.0f)
+        : GUIElement(params, timeLerpSpeed) { }
 
 
     //Adds the given element to this panel IF this panel doesn't have it already.
@@ -32,16 +20,16 @@ public:
     //Gets whether this panel contains the given GUI element.
     bool ContainsElement(GUIElementPtr element) const;
 
-    //Recalculates the position/extents of this panel.
-    //Only needs to be called externally if a child GUIElement was modified.
-    void RecalcPosition(void);
-
     const std::vector<GUIElementPtr> & GetElements(void) const { return elements; }
-    std::vector<GUIElementPtr> & GetElements(void) { return elements; }
 
+
+    virtual Box2D GetBounds(void) const override;
+    virtual bool GetDidBoundsChangeDeep(void) const override;
+
+    virtual void ScaleBy(Vector2f scaleAmount) override;
+    virtual void SetScale(Vector2f newScale) override;
 
     virtual std::string Render(float elapsedTime, const RenderInfo & info) override;
-
 
     virtual void OnMouseClick(Vector2f mouse_centerOffset) override;
     virtual void OnMouseDrag(Vector2f originalPos_centerOffset,
@@ -58,5 +46,4 @@ private:
 
     //The GUI elements inside this panel.
     std::vector<GUIElementPtr> elements;
-    Vector2f pos, extents;
 };
