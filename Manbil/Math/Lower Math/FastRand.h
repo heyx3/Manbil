af343ae3848539311a042d6f63f3c047712e403d
heyx3 <manning.w@husky.neu.edu>
2014-12-09 00:45:57 -0500

    Updated FastRand for documentation

--- Manbil/Math/Lower Math/FastRand.h
+++ Manbil/Math/Lower Math/FastRand.h
@@ -4,26 +4,29 @@
 
 
 //A fast (but not cryptographically-strong) PRNG.
+//Always generates non-negative numbers.
 class FastRand
 {
 public:
 
 	int Seed;
 
-	FastRand(int seed = 12345) : Seed(seed) { }
+	FastRand(int seed = 1234567) : Seed(seed) { }
 
 	inline int GetRandInt(void)
 	{
+        //I forget where this particular XORshift generator came from.
 		Seed = (Seed ^ 61) ^ (Seed >> 16);
 		Seed += (Seed << 3);
 		Seed ^= (Seed >> 4);
 		Seed *= 0x27d4eb2d;
 		Seed ^= (Seed >> 15);
+        assert(Seed >= 0);
 		return Seed;
 	}
 	inline float GetZeroToOne(void)
-	{
-		const int b = 999999;
-		return (float)(BasicMath::Abs(GetRandInt()) % b) / (float)b;
+    {
+        const int b = 9999999;
+		return (float)(GetRandInt() % b) / (float)b;
 	}
 };
