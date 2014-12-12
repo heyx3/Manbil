fbe45a1fd60e772cca7697c0058f85b3c0719fbd
heyx3 <manning.w@husky.neu.edu>
2014-12-11 22:09:29 -0500

    Fixed BumpmapToNormalmap

--- Manbil/Math/Higher Math/BumpmapToNormalmap.cpp
+++ Manbil/Math/Higher Math/BumpmapToNormalmap.cpp
@@ -2,22 +2,22 @@
 
 void BumpmapToNormalmap::Convert(const Array2D<float> & heightmap, float heightScale, bool normalizeRange, Array2D<Vector3f> & normals)
 {
-    //TODO: Fix.
-
     Vector3f sum, tempSum, base, first, second;
-    for (Vector2u loc(0, 1); loc.y < heightmap.GetHeight(); ++loc.y)
+    for (Vector2u loc(0, 0); loc.y < heightmap.GetHeight(); ++loc.y)
     {
         base.y = (float)loc.y;
 
-        for (loc.x = 1; loc.x < heightmap.GetWidth(); ++loc.x)
+        for (loc.x = 0; loc.x < heightmap.GetWidth(); ++loc.x)
         {
             sum = Vector3f();
 
             base.x = (float)loc.x;
             base.z = heightScale * heightmap[loc];
 
-            first = Vector3f(base.x - 1.0f, base.y, heightScale * heightmap[heightmap.Wrap(loc.LessX())]);
-            second = Vector3f(base.x, base.y - 1.0f, heightScale * heightmap[heightmap.Wrap(loc.LessY())]);
+            Vector2i locI = ToV2i(loc);
+
+            first = Vector3f(base.x - 1.0f, base.y, heightScale * heightmap[ToV2u(heightmap.Wrap(locI.LessX()))]);
+            second = Vector3f(base.x, base.y - 1.0f, heightScale * heightmap[ToV2u(heightmap.Wrap(locI.LessY()))]);
 
             tempSum = (first - base).Normalized().Cross((second - base).Normalized()).Normalized();
             if (tempSum.z < 0.0f)
@@ -26,8 +26,8 @@ void BumpmapToNormalmap::Convert(const Array2D<float> & heightmap, float heightS
             sum += tempSum;
 
 
-            first = Vector3f(base.x + 1.0f, base.y, heightScale * heightmap[heightmap.Wrap(loc.MoreX())]);
-            second = Vector3f(base.x, base.y + 1.0f, heightScale * heightmap[heightmap.Wrap(loc.MoreY())]);
+            first = Vector3f(base.x + 1.0f, base.y, heightScale * heightmap[ToV2u(heightmap.Wrap(locI.MoreX()))]);
+            second = Vector3f(base.x, base.y + 1.0f, heightScale * heightmap[ToV2u(heightmap.Wrap(locI.MoreY()))]);
 
             tempSum = (first - base).Normalized().Cross((second - base).Normalized()).Normalized();
             if (tempSum.z < 0.0f)
