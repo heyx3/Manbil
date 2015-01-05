8d0f94feb5b68975d33550352774ac5060d44227
heyx3 <manning.w@husky.neu.edu>
2015-01-04 23:21:41 -0500

    Fixed up BumpmapToNormalmap for documentation

--- Manbil/Math/Higher Math/BumpmapToNormalmap.cpp
+++ Manbil/Math/Higher Math/BumpmapToNormalmap.cpp
@@ -1,23 +1,42 @@
 #include "BumpmapToNormalmap.h"
 
-void BumpmapToNormalmap::Convert(const Array2D<float> & heightmap, float heightScale, bool normalizeRange, Array2D<Vector3f> & normals)
+void BumpmapToNormalmap::Convert(const Array2D<float>& heightmap, float heightScale,
+                                 bool normalizeRange, Array2D<Vector3f>& normals)
 {
-    Vector3f sum, tempSum, base, first, second;
+    //Go through every point on the bumpmap and find its normal.
+    Vector3f base, first, second;
     for (Vector2u loc(0, 0); loc.y < heightmap.GetHeight(); ++loc.y)
     {
         base.y = (float)loc.y;
 
         for (loc.x = 0; loc.x < heightmap.GetWidth(); ++loc.x)
         {
-            sum = Vector3f();
-
             base.x = (float)loc.x;
+
             base.z = heightScale * heightmap[loc];
 
+
+            //Get the two normals that define this point's normal and average them together.
+
+            Vector3f sum;
             Vector2i locI = ToV2i(loc);
 
-            first = Vector3f(base.x - 1.0f, base.y, heightScale * heightmap[ToV2u(heightmap.Wrap(locI.LessX()))]);
-            second = Vector3f(base.x, base.y - 1.0f, heightScale * heightmap[ToV2u(heightmap.Wrap(locI.LessY()))]);
+            first = Vector3f(base.x - 1.0f, base.y,
+                             heightScale * heightmap[ToV2u(heightmap.Wrap(locI.LessX()))]);
+            second = Vector3f(base.x, base.y - 1.0f,
+                              heightScale * heightmap[ToV2u(heightmap.Wrap(locI.LessY()))]);
+
+            Vector3f tempSum = (first - base).Normalized().Cross((second - base).Normalized()).Normalized();
+            if (tempSum.z < 0.0f)
+                tempSum = -tempSum;
+
+            sum += tempSum;
+
+
+            first = Vector3f(base.x + 1.0f, base.y,
+                             heightScale * heightmap[ToV2u(heightmap.Wrap(locI.MoreX()))]);
+            second = Vector3f(base.x, base.y + 1.0f,
+                              heightScale * heightmap[ToV2u(heightmap.Wrap(locI.MoreY()))]);
 
             tempSum = (first - base).Normalized().Cross((second - base).Normalized()).Normalized();
             if (tempSum.z < 0.0f)
@@ -25,20 +44,14 @@ void BumpmapToNormalmap::Convert(const Array2D<float> & heightmap, float heightS
 
             sum += tempSum;
 
+            sum.Normalize();
 
-            first = Vector3f(base.x + 1.0f, base.y, heightScale * heightmap[ToV2u(heightmap.Wrap(locI.MoreX()))]);
-            second = Vector3f(base.x, base.y + 1.0f, heightScale * heightmap[ToV2u(heightmap.Wrap(locI.MoreY()))]);
+            //Pack the normal into the range [0, 1] if necessary.
+            if (normalizeRange)
+            {
+                sum = (sum * 0.5f) + Vector3f(0.5f, 0.5f, 0.5f);
+            }
 
-            tempSum = (first - base).Normalized().Cross((second - base).Normalized()).Normalized();
-            if (tempSum.z < 0.0f)
-                tempSum = -tempSum;
-
-            sum += tempSum;
-
-
-            sum = sum.Normalized();
-
-            if (normalizeRange) sum = (sum * 0.5f) + Vector3f(0.5f, 0.5f, 0.5f);
             normals[loc] = sum;
         }
     }
--- Manbil/Math/Higher Math/BumpmapToNormalmap.h
+++ Manbil/Math/Higher Math/BumpmapToNormalmap.h
@@ -7,6 +7,8 @@ class BumpmapToNormalmap
 {
 public:
     
-    //If "normalizeRange" is true, each normal's X, Y, and Z values will be remapped from [-1, 1] to [0, 1] for packing into a texture.
-    static void Convert(const Array2D<float> & heightmap, float heightScale, bool normalizeRange, Array2D<Vector3f> & outNormals);
+    //If "normalizeRange" is true, each normal's X, Y, and Z values will be remapped
+    //    from [-1, 1] to [0, 1] for packing into a texture.
+    static void Convert(const Array2D<float>& heightmap, float heightScale,
+                        bool normalizeRange, Array2D<Vector3f>& outNormals);
 };
