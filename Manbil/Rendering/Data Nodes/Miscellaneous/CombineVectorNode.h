30ccb789b734cda5d935ed1cff17e03b37b1b7fb
heyx3 <manning.w@husky.neu.edu>
2015-03-15 17:46:59 -0400

    Small formatting fix to CombineVectorNode

--- Manbil/Rendering/Data Nodes/Miscellaneous/CombineVectorNode.h
+++ Manbil/Rendering/Data Nodes/Miscellaneous/CombineVectorNode.h
@@ -8,14 +8,14 @@ class CombineVectorNode : public DataNode
 {
 public:
 
-    virtual unsigned int GetOutputSize(unsigned int index) const override;
-    virtual std::string GetOutputName(unsigned int index) const override;
-
-
     CombineVectorNode(const DataLine & input1, const DataLine & input2, std::string name = "") : CombineVectorNode(MakeVector(input1, input2), name) { }
     CombineVectorNode(const DataLine & input1, const DataLine & input2, const DataLine & input3,  std::string name = "") : CombineVectorNode(MakeVector(input1, input2, input3), name) { }
     CombineVectorNode(const DataLine & input1, const DataLine & input2, const DataLine & input3, const DataLine & input4, std::string name = "") : CombineVectorNode(MakeVector(input1, input2, input3, input4), name) { }
     CombineVectorNode(const std::vector<DataLine> & inputs, std::string name = "");
+
+
+    virtual unsigned int GetOutputSize(unsigned int index) const override;
+    virtual std::string GetOutputName(unsigned int index) const override;
 
 
 protected:
