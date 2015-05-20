d4392da372094708a4d12ba72569c83e49b5c43d
heyx3 <manning.w@husky.neu.edu>
2015-05-20 00:58:01 -0400

    Add flexibility to GUIManager

--- Manbil/Rendering/GUI/GUIManager.h
+++ Manbil/Rendering/GUI/GUIManager.h
@@ -8,16 +8,24 @@ class GUIManager
 {
 public:
 
+    //Use either "RootElement" or "RootElementRaw".
 
     GUIElementPtr RootElement;
+    GUIElement* RootElementRaw = 0;
+
 
     //Creates a manager with the given root GUIElement.
     GUIManager(GUIElementPtr root = GUIElementPtr(0)) : RootElement(root) { }
+    GUIManager(GUIElement* root) : RootElementRaw(root) { }
 
     //Updates the root element and sends it the appropriate mouse input message.
     void Update(float elapsed, Vector2i mousePos, bool clicked);
     //Renders the root element.
     void Render(float elapsed, const RenderInfo& info);
+
+    GUIElement* GetRoot(void) const { return (RootElementRaw == 0 ? RootElement.get() : RootElementRaw); }
+    void SetRoot(GUIElement* rawRoot) { RootElement.reset(); RootElementRaw = rawRoot; }
+    void SetRoot(GUIElementPtr root) { RootElementRaw = 0; RootElement = root; }
 
 
 private:
